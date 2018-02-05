553c8de50d9cfb8b4aa5165ee7a2025270ad4ebf
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2018-02-05 16:43:21 -0500

    see the folder now

---
+++ lab/frequencies/app.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(DT)
+library(MOTE)
+options(scipen = 999)
+
+doublewordtable = read.csv("../DoubleWordTab.csv")
+doubleworddata = read.csv("../double_words.csv")
+
+##fix this part here when the new values are avaliable 
+doublewordtable$Minimum[6:10] = c(apa(apply(doubleworddata[ , 3:7], 2, min, na.rm = T), 2))
+doublewordtable$Maximum[6:10] = c(apa(apply(doubleworddata[ , 3:7], 2, max, na.rm = T), 2))
+doublewordtable$M[6:10] = c(apa(apply(doubleworddata[ , 3:7], 2, mean, na.rm = T), 2))
+doublewordtable$SD[6:10] = c(apa(apply(doubleworddata[ , 3:7], 2, sd, na.rm = T), 2))
+
+
+ui = fluidPage(
+  titlePanel("Double Words Variable List"),
+  DTOutput('tbl'))
+
+server = function(input, output) {
+  output$tbl = renderDT(
+    doublewordtable, 
+    extensions = 'Responsive',
+    options = list(
+      dom = 'tp'
+    ),
+    rownames = FALSE
+    
+  )
+}
+
+shinyApp(ui, server)
