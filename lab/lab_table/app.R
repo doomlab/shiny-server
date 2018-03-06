64a9d4eb50905ff313824029f61b82e96f5b33b4
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2018-03-06 10:52:49 -0500

    did some table updates

--- lab/lab_table/app.R
+++ lab/lab_table/app.R
@@ -3,23 +3,33 @@ library(DT)
 library(MOTE)
 options(scipen = 999)
 
-doublewordtable = read.csv("../DoubleWordTab.csv")
-doubleworddata = read.csv("../double_words.csv")
+#load data
+labtable = read.csv("../lab_table.csv")
 
-##fix this part here when the new values are avaliable 
-doublewordtable$Minimum[3:10] = c(apa(apply(doubleworddata[ , 3:10], 2, min, na.rm = T), 2))
-doublewordtable$Maximum[3:10] = c(apa(apply(doubleworddata[ , 3:10], 2, max, na.rm = T), 2))
-doublewordtable$M[3:10] = c(apa(apply(doubleworddata[ , 3:10], 2, mean, na.rm = T), 2))
-doublewordtable$SD[3:10] = c(apa(apply(doubleworddata[ , 3:10], 2, sd, na.rm = T), 2))
+#clean up journal information
+labtable$Full.Reference = with(labtable, paste(author, year, ref_title,
+                                            ref_journal, ref_volume, 
+                                            ref_page, ref_doi, sep = ". "))
+#clean up the tags
+labtable$Tag.List = apply(labtable[ , 21:66] == 1, 1, function(x) { toString(names(which(x))) } )
 
+#clean up the stimuli
+labtable$Stimuli.List = paste(labtable$no1, " ", labtable$type1, ", ",
+                              labtable$no2, " ", labtable$type2, sep = "")
+
+labtable$Stimuli.List = gsub("NA", "", labtable$Stimuli.List, ignore.case = F)
+labtable$Stimuli.List = gsub(",\\s\\s$", "", labtable$Stimuli.List, ignore.case = F)
+
+#selectizeInput(
+#  'e2', '2. Multi-select', choices = state.name, multiple = TRUE),
 
 ui = fluidPage(
-  titlePanel("Double Words Variable List"),
+  titlePanel("LAB Table List"),
   DTOutput('tbl'))
 
 server = function(input, output) {
   output$tbl = renderDT(
-    doublewordtable, 
+    labtable[ , 76:78], 
     extensions = 'Responsive',
     options = list(
       dom = 'tp'
