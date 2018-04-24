1fd72026f8e3f827c6803405920049fb81ea10de
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2018-04-23 20:10:40 -0400

    updates

--- MOTE/about_page.R
+++ MOTE/about_page.R
@@ -7,8 +7,8 @@ fluidRow(
 Dr. Erin M. Buchanan - check her out at <a href=\"http://www.aggieerin.com\">her website</a>.<br>
 Amber Gillenwaters<br>
 William Padfield<br>
-Addie Wikowskey<br>
 Abigail Van Nuland<br>
+Addie Wikowsky - I am a current graduate student at Missouri State University. I am working towards a Master's in Experimental Psychology. I received my Bachelors of Science in Psychology (with a minor in Biology) from Missouri Western State University where I learned to grow as a researcher. Besides academics, I enjoy spending time hiking with my two dogs, Elliot and Trout. <br>
 All questions or comments can be directed to <a href=\"mailto:buchananlab@gmail.com\">the lab gmail.</a>")) ## close column
   
 ) ##close fluid row
