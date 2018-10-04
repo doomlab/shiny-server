6108b1e198e0daf8c1927fed747463c18b77a63f
Amber Gillenwaters <34293640+Gillenwaters@users.noreply.github.com>
2018-10-03 23:23:53 -0500

    dropdown menu

--- scip_wordspace/app.R
+++ scip_wordspace/app.R
@@ -1,50 +1,45 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(lsa)
+library(LSAfun)
+library(haven)
+library(readr)
 
-# Define UI for application that draws a histogram
+load("TASA.rda")
+load("EN_100k.rda")
+load("EN_100k_lsa.rda")
+
+# Define UI
 ui <- fluidPage(
-    
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-    
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # Application title
+  
+  titlePanel("Word Visualization"),
+  selectInput("dataset", "Data Set:",
+              c("English LSA Space: TASA" = "TASA",
+                "English HAL Space: BNC/ukWaC/Wikipedia" = "EN",
+                "English LSA Space: BNC/ukWaC/Wikipedia" = "ENLSA")
+  
+
     )
+      
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
+
+TASA <- load("TASA.rda")
+EN = load("EN_100k.rda")
+ENLSA = load("EN_100k_lsa.rda")
+
 server <- function(input, output) {
-    
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2] 
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
+  
+
+  }
+
+
+
+
+
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
