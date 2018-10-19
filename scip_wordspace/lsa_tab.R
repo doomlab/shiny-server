78b66e772ddcd9380a69f6f8378aaaa28658fe2d
williampadfield <35707688+williampadfield@users.noreply.github.com>
2018-10-19 09:29:23 -0500

    Added more to lsa tab

--- scip_wordspace/lsa_tab.R
+++ scip_wordspace/lsa_tab.R
@@ -1,6 +1,10 @@
 lsa_tab <- tabItem(tabName = "lsa_tab",
                        fluidRow(
                          titlePanel("LSA"),
-                         mainPanel(DTOutput("lsa_table"))
+                         selectInput("rownames_select", "Select Row Names:",
+                                     rownames(import_lsa)
+                                     ), #close selectInput
+                         numericInput("neighbors", "Neighbors:", 10, min = 1, max = 100),
+                         mainPanel(DTOutput("lsa_data.table"))
                        ) #close fluidRow
 ) #close tabItem
