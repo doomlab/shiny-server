9497ad48927a05ad578e863b21a3e3c3e2a4337d
Ari Cunningham <arielle924@gmail.com>
2018-10-26 13:30:04 -0500

    added headings to lsa_tab_single

--- scip_wordspace/lsa_tab_single.R
+++ scip_wordspace/lsa_tab_single.R
@@ -10,10 +10,10 @@ lsa_tab_single <- tabItem(tabName = "lsa_tab_single",
                                      choices = rownames(import_lsa)
                                      ), #close selectInput
                          
-                         numericInput("neighbors", "Neighbors:", 10, min = 1, max = 100),
+                         numericInput("neighbors", "Number of Neighbors to Display:", 10, min = 1, max = 100),
                          
                          sliderInput(inputId = "select_range", 
-                                     label = "Select range:", 
+                                     label = "Set Cosine Range:", 
                                      min = -1, 
                                      max = 1,
                                      value = c(-1,1),
@@ -22,9 +22,10 @@ lsa_tab_single <- tabItem(tabName = "lsa_tab_single",
                          ), #close main panel 
                          
                          mainPanel(
-                           
+                           h4("Plot of Nearest Neighbors"),
                            plotOutput("lsa_plotneighbors"),
                            br(),
+                           h4("Neighbors Within Selected Cosine Range"),
                            DTOutput("lsa_choosetarget")
                            ) #close main panel
                          ) #close fluidRow
