cc8587288840c30b898721d03c3bb1725fbc9aa2
addwiko <35707681+addwiko@users.noreply.github.com>
2018-10-30 10:33:20 -0500

    more text/moved text

--- scip_wordspace/lsa_tab_multiple.R
+++ scip_wordspace/lsa_tab_multiple.R
@@ -2,18 +2,20 @@
 lsa_tab_multiple <- tabItem(tabName = "lsa_tab_multiple",
                         fluidRow(
                           
+                          
+                          p("Please choose a minimum of three tags for model to work."),
                           mainPanel(
                            
                             titlePanel("LSA Multiple Word Select"),
                             selectizeInput("tags", "Select Tags:", 
                                            choices = c(rownames(import_lsa)), 
                                            multiple = TRUE), #close selectizeInput
-                            p("Please choose a minimum of three tags for model to work."),
+                            p("This graph is demonstrating how close the different tags choosen are related relative to work neighborhoods."),
                             DTOutput("lsa_multicos"), 
-                            p("Explain what is going on in the graph"),
                             br(),
                             
                             plotOutput("lsa_plotwordlist")
+                            
                             ) #close main panel
                           ) #close fluidRow
                         ) #close tabItem
