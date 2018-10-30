d9cce94d682884d9116296bb0c0c1c135b46d78c
addwiko <35707681+addwiko@users.noreply.github.com>
2018-10-30 10:19:06 -0500

    added warning for min. tags/slight description

--- scip_wordspace/lsa_tab_multiple.R
+++ scip_wordspace/lsa_tab_multiple.R
@@ -3,11 +3,14 @@ lsa_tab_multiple <- tabItem(tabName = "lsa_tab_multiple",
                         fluidRow(
                           
                           mainPanel(
+                           
                             titlePanel("LSA Multiple Word Select"),
                             selectizeInput("tags", "Select Tags:", 
                                            choices = c(rownames(import_lsa)), 
                                            multiple = TRUE), #close selectizeInput
+                            p("Please choose a minimum of three tags for model to work."),
                             DTOutput("lsa_multicos"), 
+                            p("Explain what is going on in the graph"),
                             br(),
                             
                             plotOutput("lsa_plotwordlist")
